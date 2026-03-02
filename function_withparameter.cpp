c5191d886d22ab6724eeccae423f28cee22bc445
Hirzee <faruqhirzi698@gmail.com>
2026-03-02 12:49:38 +0700

    function_withparameter_output

--- function_withparameter.cpp
+++ function_withparameter.cpp
@@ -11,3 +11,19 @@ void input()
     cin >> lebar;
 }
 
+int luaspersegi(int a, int b)
+{
+    return a * b;
+}
+
+int kelilingpersegi(int a, int b)
+{
+    return 2 * (a + b);
+}
+
+void output()
+{
+    cout << "Luas persegi: " << luaspersegi(panjang, lebar) << endl;
+    cout << "Keliling persegi: " << kelilingpersegi(panjang, lebar) << endl;
+}
+
