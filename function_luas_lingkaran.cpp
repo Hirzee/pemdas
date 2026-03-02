9d49fe2d46f8140c0dbc8a25113d1888d0f09d73
Hirzee <faruqhirzi698@gmail.com>
2026-03-02 12:41:40 +0700

    function_luas_lingkaran_input

---
+++ function_luas_lingkaran.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+float r;
+float phi = 3.14;
+
+void input()
+{
+    cout << "Isi jari jari : ";
+    cin >> r;
+}
+
+float luas(float a)
+{
+    return phi * a * a;
+}
